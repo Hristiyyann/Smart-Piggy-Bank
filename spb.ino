a4ae7b76100dfdf92e0de1f0bc280e4bd5a41ba9
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-03-31 00:31:52 +0300

    lcd code added and fixes on the arduino

--- spb.ino
+++ spb.ino
@@ -2,12 +2,13 @@
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 
-#define led10 53
-#define led20 51
+#define led10 39
+#define led20 42
 #define led2 52
-#define led1 50
-#define led50 48
+#define led1 48
+#define led50 45
 
+float money_insert = 0;
 Servo moneyservo; 
 
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); 
@@ -15,14 +16,17 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 void setup() {
   moneyservo.attach(9);
   moneyservo.write(0);
+
+  lcd.init();
+  lcd.backlight();
   
   Serial.begin(9600);
   pinMode(led10, OUTPUT); 
   digitalWrite(led10, HIGH);
-  
+   
   pinMode(led20, OUTPUT); 
   digitalWrite(led20, HIGH);
-
+ 
   pinMode(led50, OUTPUT); 
   digitalWrite(led50, HIGH);
 
@@ -48,44 +52,56 @@ void loop() {
   int value30 = analogRead(A11);
   int value40 = analogRead(A12);
 
-  Serial.println(value5);
+ /* Serial.println(value5);
   Serial.println(value10);
   Serial.println(value20);
   Serial.println(value30);
   Serial.println(value40);
 
-  delay(5000);
+  delay(5000);*/
   
   if(valuefor2 < 900)
   {
+    money_insert += 2.0;
     Serial.println("2 leva");
     delay(250); 
   }
   
   else if(valuefor1 < 900)
   {
+    money_insert += 1.0; 
     Serial.println("1 leva");
     delay(250); 
   }
   
   else if(valuefor50 < 900)
   {
+    money_insert += 0.5;
     Serial.println("50 st");
     delay(250); 
   }
   
   else if(valuefor20 < 900)
   {
+    money_insert += 0.2;
     Serial.println("20 st");
     delay(250); 
   }
 
   else if(valuefor10 < 900)
   {
+    money_insert += 0.1;
     Serial.println("10 st");
     delay(250); 
   }
 
+  lcd.setCursor(1, 0); 
+  lcd.print("Current money:"); 
+  lcd.setCursor(2, 1);
+  lcd.print(money_insert);
+  lcd.setCursor(9  , 1);
+  lcd.print("leva.");
+  
   int degree;
   delay(1000);
   
